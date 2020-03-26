7451ed7526722133c0a0b1a8fbde41d0e763e421
maochunsun <maochuns.sun@gmail.com>
2020-03-26 18:05:26 +0800

    Update SecuXAccountManager.swift

--- secux-paymentkit/Classes/SecuXAccountManager.swift
+++ secux-paymentkit/Classes/SecuXAccountManager.swift
@@ -15,6 +15,10 @@ open class SecuXAccountManager{
         
     }
     
+    public func setBaseServer(url:String){
+        SecuXServerRequestHandler.baseURL = url
+    }
+    
     public func registerUserAccount(userAccount: SecuXUserAccount, coinType: String, token: String) -> (SecuXRequestResult, Data?){
         logw("registerUserAccount")
         let (ret, data) = secuXSvrReqHandler.userRegister(userAccount: userAccount, coinType: coinType, token: token)
@@ -68,6 +72,23 @@ open class SecuXAccountManager{
                 return (SecuXRequestResult.SecuXRequestFailed, "Invalid json response from server".data(using: String.Encoding.utf8))
             }
             
+            if let phone = responseJson["tel"] as? String{
+                userAccount.phone = phone
+            }
+            
+            if let email = responseJson["email"] as? String{
+                userAccount.email = email
+            }
+            
+            if let name = responseJson["name"] as? String{
+                userAccount.alias = name
+            }
+            
+            if let type = responseJson["userType"] as? String{
+                userAccount.type = type
+            }
+            
+            /*
             guard let coinType = responseJson["coinType"] as? String,
                 let token = responseJson["symbol"] as? String,
                 let balance = responseJson["balance"]  as? Double,
@@ -83,9 +104,6 @@ open class SecuXAccountManager{
             let formattedBalDec = Decimal(formattedBalance)
             let usdBalDec = Decimal(usdBalance)
             
-            if let phone = responseJson["tel"], let phoneStr = phone as? String{
-                userAccount.phone = phoneStr
-            }
             
             let tokenBalance = SecuXCoinTokenBalance(balance: balDec, formattedBalance: formattedBalDec, usdBalance: usdBalDec)
             
@@ -94,9 +112,9 @@ open class SecuXAccountManager{
             
             let _ = SecuXCoinAccount(type: coinType, name: token, tokenBalDict: dict)
             
-            //userAccount.coinAccountArray = [SecuXCoinAccount]()
-            //userAccount.coinAccountArray.append(coinAccount)
-            
+            userAccount.coinAccountArray = [SecuXCoinAccount]()
+            userAccount.coinAccountArray.append(coinAccount)
+            */
             return (ret, nil)
             
         }
@@ -104,8 +122,36 @@ open class SecuXAccountManager{
         return (ret, data)
     }
     
-    public func getSupportedCoinTokenArray() -> (SecuXRequestResult, Data?){
-        return secuXSvrReqHandler.getSupportedCoinTokens()
+    public func getSupportedCoinTokenArray() -> (SecuXRequestResult, Data?, [(coin:String, token:String)]?){
+        let (ret, data) = secuXSvrReqHandler.getSupportedCoinTokens()
+        guard ret == SecuXRequestResult.SecuXRequestOK, let replyData = data else{
+            return (ret, data, nil)
+        }
+            
+        guard let responseArr = try? JSONSerialization.jsonObject(with: replyData, options: []) as? [[String]] else{
+            return (SecuXRequestResult.SecuXRequestFailed, "Invalid json response from server".data(using: String.Encoding.utf8), nil)
+        }
+        
+        var coinTokenArray = [(coin:String, token:String)]()
+        for item in responseArr{
+            
+            if item.count == 2{
+            
+                let coin = item[0]
+                let token = item[1]
+                print("coin = \(coin) token = \(token)")
+                
+                coinTokenArray.append((coin:coin, token:token))
+            }else{
+                print("Invalid coin token info.")
+            }
+        }
+        
+        guard coinTokenArray.count > 0 else{
+            return (SecuXRequestResult.SecuXRequestFailed, "No supported coin & token info. from server".data(using: String.Encoding.utf8), nil)
+        }
+        
+        return (ret, data, coinTokenArray)
     }
     
     public func getCoinAccountList(userAccount:SecuXUserAccount) -> (SecuXRequestResult, Data?){
